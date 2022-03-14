fafa2216e3052ee4a2e0b36f20d3a46bae95c668
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2022-03-14 23:08:53 +0800

    Delete login directory

--- login/vue.config.js
+++
@@ -1,7 +0,0 @@
-const {defineConfig} = require('@vue/cli-service')
-module.exports = defineConfig({
-    transpileDependencies: true,
-    devServer: {
-        port: 8081, // 不与java后端端口号8080冲突
-    }
-})
